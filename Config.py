16861b645f5e977ef13fe03770a453fea4831428
bayuwicita <109690253+bayuwicita@users.noreply.github.com>
2022-12-16 10:16:23 +0700

    Update Config.py

--- Config.py
+++ Config.py
@@ -16,17 +16,7 @@ class Config():
   SUDO_USERS = list(set(SUDO_USERS))
 
 class Messages():
-      HELP_MSG = [
-        ".",
+  
+      SC_MSG = "**Halo [{}](tg://user?id={})**\n click on below👇 button to get my source code, for more help ask in my support group👇👇 "
 
-        "**Force Subscribe**\n__Force group members to join a specific channel before sending messages in the group.\nI will mute members if they not joined your channel and tell them to join the channel and unmute themself by pressing a button.__",
-        
-        "**Setup**\n__First of all add me in the group as admin with ban users permission and in the channel as admin.\nNote: Only creator of the group can setup me and i will leave the chat if i am not an admin in the chat.__",
-        
-        "**Commmands**\n__/ForceSubscribe - To get the current settings.\n/ForceSubscribe no/off/disable - To turn of ForceSubscribe.\n/ForceSubscribe {channel username or channel ID} - To turn on and setup the channel.\n/ForceSubscribe clear - To unmute all members who muted by me.\n/source_code - To get bot source code😍\n\nNote: /FSub is an alias of /ForceSubscribe__",
-        
-       "**Devloped By @UniversalBotsUpdate**"
-      ]
-      SC_MSG = "**Hey [{}](tg://user?id={})**\n click on below👇 button to get my source code, for more help ask in my support group👇👇 "
-
-      START_MSG = "**Hey [{}](tg://user?id={})**\n__I can force members to join a specific channel before writing messages in the group.\nLearn more at /help__"
+      START_MSG = "**Halo [{}](tg://user?id={})\n\nAgar dapat menggunakan NekoPoi Bot\nanda diharuskan untuk bergabung di channel NekoPoi terlebih dahulu.\nJika sudah bergabung klik /start**"
